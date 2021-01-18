bb2227be46f2f413aed1ef9350a4fd553aaa7ed7
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2021-01-18 15:37:03 +0900

    #18.0 Getting Authorization Code

--- users/views.py
+++ users/views.py
@@ -127,3 +127,26 @@ def github_callback(request):
     except GithubException:
         # send Error Message
         return redirect(reverse("users:login"))
+
+
+def kakao_login(request):
+    REST_API_KEY = os.environ.get("KAKAO_ID")
+    REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code"
+    )
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}"
+        )
+    except KakaoException:
+        return redirect(reverse("users:login"))
